d4f93b4bf374dc90c98e04e2a5e482a3bebe558d
Yash Jha <yashjha00000@gmail.com>
2025-05-01 18:08:12 +0530

    fixed bugs

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -11,5 +11,5 @@ const route = express.Router();
 route.post("/signup", singup);
 route.post("/signin", signin);
 route.post("/google", googleSignIn);
-route.get("/signout", signOut);
+route.post("/signout", signOut);
 export default route;
