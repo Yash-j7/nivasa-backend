4fa40d367a35aa1a60a947ed998753ff4bb8efda
Yash Jha <yashjha00000@gmail.com>
2025-05-01 21:01:53 +0530

    verifyToken

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -9,11 +9,9 @@ import { verifyToken } from "../utils/varifyUser.js";
 
 const router = express.Router();
 
-router.get("/:id",  getUser);
+router.get("/:id", getUser);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/posts/:id", verifyToken, userPosts);
-
-
+router.get("/posts/:id", userPosts);
 
 export default router;
